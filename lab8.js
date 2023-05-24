459726a597732d6ebf3773cf07bb832b6b7a30c9
Denis-Duhov <denishomeworke@mail.ru>
2023-05-24 11:59:15 +0700

    Время

--- lab8.js
+++ lab8.js
@@ -36,3 +36,11 @@ function clearInput() {
     inputDate.value = ""
     result.innerHTML = ""
 }
+
+function showTime() {
+    let today = new Date();
+    let currentTime = today.toLocaleTimeString('ru-RU');
+    document.getElementById('time').innerHTML = currentTime;
+}
+
+setInterval(showTime, 1000);
